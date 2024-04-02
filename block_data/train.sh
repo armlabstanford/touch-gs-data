a6e76de8a0cba7abe1957bd8ef24141e1a45026c
Matt Strong <mastro1@stanford.edu>
2024-04-02 14:45:57 -0700

    first batch of changes

--- block_data/train.sh
+++ block_data/train.sh
@@ -2,7 +2,12 @@
 
 echo "Starting train script for block data"
 
-python3 utils/read_realsense_depth.py --base_repo_path block_data
+# python3 utils/read_realsense_depth.py --base_repo_path block_data
 
 
-python3 utils/read_touch_depths.py --base_repo_path block_data
+# python3 utils/read_touch_depths.py --base_repo_path block_data
+
+
+python3 utils/fuse_touch_vision.py --root_dir 'block_data' --aligning_depths 'realsense_depths' --touch_depth 'touch_depth' --zoe_depth_path 'blocks_zoe_depth' --vision_output_dir 'vision' --fused_output_dir 'fused_output_dir' --touch_var 'touch_var'
+
+# python3 utils/fuse_touch_vision.py --root_dir 'block_data' --aligning_depths 'realsense_depths' --touch_depth 'touch_depth' --zoe_depth_path 'blocks_zoe_depth' --viz --vision_output_dir 'vision' --fused_output_dir 'fused_output_dir' --touch_var 'touch_var'

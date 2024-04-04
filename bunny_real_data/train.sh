f0e8ed5b885b4a9cdbe16e09d574ce0b260af4b8
Matt Strong <mastro1@stanford.edu>
2024-04-03 17:42:40 -0700

    batch 6

---
+++ bunny_real_data/train.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "Starting train script for bunny real data"
+
+
+echo "Reading realsense depths..."
+echo 
+sleep 1
+
+python3 utils/read_realsense_depth.py --base_repo_path bunny_real_data
+
+echo "Reading touch depths..."
+echo 
+sleep 1
+
+python3 utils/read_touch_depths.py --base_repo_path bunny_real_data
+
+
+echo "Fusing vision and touch..."
+echo 
+sleep 1
+
+# python3 utils/fuse_touch_vision.py --root_dir 'block_data' --aligning_depths 'realsense_depths' --touch_depth 'touch_depth' --zoe_depth_path 'blocks_zoe_depth' --vision_output_dir 'vision' --fused_output_dir 'fused_output_dir' --touch_var 'touch_var'
+
+python3 utils/fuse_touch_vision.py --root_dir 'bunny_real_data' --aligning_depths 'realsense_depths' --touch_depth 'touch_depth' --zoe_depth_path 'bunny_zoe_real_world_depth' --use_uncertainty --vision_output_dir 'vision' --fused_output_dir 'fused_output_dir' --touch_var 'touch_var'
+
+echo "Adding depths and uncertainties to transforms.json..."
+echo 
+sleep 1
+
+python3 utils/add_depth_file_path_to_transforms.py --base_repo_path 'bunny_real_data' --filename 'transforms.json' --depth_file_path_template 'fused_output_dir' --uncertainty_file_path_template 'fused_output_dir_uncertainty'
